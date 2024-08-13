18380aa4e75f5973efa9e033117b1f31181df791
Juan Esaul <esauldd@gmail.com>
2024-08-13 13:49:14 -0600

    Simulaciones

--- simulaciones.py
+++ simulaciones.py
@@ -128,6 +128,8 @@ if __name__ == "__main__":
     np.random.seed(57)
 
     # Examples
+
+    # Dyson and deterministic Dyson comparison
     n = 9
     time_dyson = 20
     dt = 0.01
@@ -136,30 +138,112 @@ if __name__ == "__main__":
     sims = dyson(dt=dt,time=time_dyson,n=n,starting=starting)
     det_sims = deterministic_dyson(dt=dt,time=time_dyson,n=n,starting=starting)
     for i in range(n):
-        plt.plot(time_mesh,det_sims[:,i])
+        plt.plot(time_mesh,det_sims[:,i],linewidth=0.8)
     for i in range(n):
-        plt.plot(time_mesh,sims[:,i])
+        plt.plot(time_mesh,sims[:,i],linewidth=1)
     plt.xlabel(r"Time ($t$)")
     plt.ylabel(r"Position in space ($\lambda_i$)")
     plt.show()
+    # plt.savefig("img/explot.pgf")
+
+    # Examples of Dyson Brownian motion
+    dys_fig, dys_ax = plt.subplots(2,2)
+    dys_fig.set_size_inches(7,7)
+    n_ex2 = 4
+    dt_ex2 =0.01
+    time_ex2 = 10
+    starting_ex2 = np.array( [-1.1,-1,2,2.1] )
     
-    # A distance does not grow
-    # plt.figure(figsize=(7,4))
-    # n_ex2 = 4
-    # starting_ex2 = np.array( [-1.1,-1,2,2.1] )
-    # sims_ex2 = deterministic_dyson(dt=0.01,time=10,n=n_ex2,starting=starting_ex2)
-    # for i in range(n_ex2):
-    #     plt.plot(sims_ex2[:,i])
-    # plt.show()
+    # First experiment
+    time_mesh_ex2 = np.linspace(start=0,stop=time_ex2,num=int(time_ex2/dt_ex2))
+    sims_ex2 = dyson(dt=dt_ex2,time=time_ex2,n=n_ex2,starting=starting_ex2)
+    for i in range(n_ex2):
+        dys_ax[0,0].plot(time_mesh_ex2,sims_ex2[:,i],linewidth=0.6)
 
+    # Second experiment
+    dt_ex3 = 0.01
+    n_ex3 = 5
+    starting_ex3 = np.linspace(start=-1,stop=1,num=n_ex3)
+    time_mesh_ex3 = np.linspace(start=0,stop=time_ex2,num=int(time_ex2/dt_ex3))
+    sims_ex3 = dyson(dt=dt_ex3,time=time_ex2,n=n_ex3,starting=starting_ex3)
+    for i in range(n_ex3):
+        dys_ax[0,1].plot(time_mesh_ex3,sims_ex3[:,i],linewidth=0.6)
 
+    # Third experiment
+    dt_ex4 = 0.01
+    n_ex4 = 7
+    starting_ex4 = np.linspace(start=-2.5,stop=2.5,num=n_ex4)
+    time_mesh_ex4 = np.linspace(start=0,stop=time_ex2,num=int(time_ex2/dt_ex4))
+    sims_ex4 = dyson(dt=dt_ex4,time=time_ex2,n=n_ex4,starting=starting_ex4)
+    for i in range(n_ex4):
+        dys_ax[1,0].plot(time_mesh_ex4,sims_ex4[:,i],linewidth=0.6)
+
+    # Fourth experiment
+    dt_ex5 = 0.001
+    n_ex5 = 9
+    starting_ex5 = np.linspace(start=-2.5,stop=2.5,num=n_ex5)
+    time_mesh_ex5 = np.linspace(start=0,stop=time_ex2,num=int(time_ex2/dt_ex5))
+    sims_ex5 = dyson(dt=dt_ex5,time=time_ex2,n=n_ex5,starting=starting_ex5)
+    for i in range(n_ex5):
+        dys_ax[1,1].plot(time_mesh_ex5,sims_ex5[:,i],linewidth=0.6)
+    # Figure parameters
+    for ax in dys_ax.flat:
+        ax.set(xlabel=r'time ($t$)', ylabel=r'Position')
+    for ax in dys_ax.flat:
+        ax.label_outer()
+    
+    # deterministic Dyson
+    # A distance that does not grow
+    det_dys_fig, det_dys_ax = plt.subplots(2,2)
+    det_dys_fig.set_size_inches(7,7)
+    n_ex2 = 4
+    dt_ex2 =0.01
+    time_ex2 = 10
+    starting_ex2 = np.array( [-1.1,-1,2,2.1] )
+    time_mesh_ex2 = np.linspace(start=0,stop=time_ex2,num=int(time_ex2/dt_ex2))
+    sims_ex2 = deterministic_dyson(dt=dt_ex2,time=time_ex2,n=n_ex2,starting=starting_ex2)
+    for i in range(n_ex2):
+        det_dys_ax[0,0].plot(time_mesh_ex2,sims_ex2[:,i])
+    # Second experiment
+    dt_ex3 = 0.01
+    n_ex3 = 5
+    starting_ex3 = np.linspace(start=-1,stop=1,num=n_ex3)
+    time_mesh_ex3 = np.linspace(start=0,stop=time_ex2,num=int(time_ex2/dt_ex3))
+    sims_ex3 = deterministic_dyson(dt=dt_ex3,time=time_ex2,n=n_ex3,starting=starting_ex3)
+    for i in range(n_ex3):
+        det_dys_ax[0,1].plot(time_mesh_ex3,sims_ex3[:,i])
+
+    # Third experiment
+    dt_ex4 = 0.01
+    n_ex4 = 7
+    starting_ex4 = np.linspace(start=-2.5,stop=2.5,num=n_ex4)
+    time_mesh_ex4 = np.linspace(start=0,stop=time_ex2,num=int(time_ex2/dt_ex4))
+    sims_ex4 = deterministic_dyson(dt=dt_ex4,time=time_ex2,n=n_ex4,starting=starting_ex4)
+    for i in range(n_ex4):
+        det_dys_ax[1,0].plot(time_mesh_ex4,sims_ex4[:,i])
+
+    # Fourth experiment
+    dt_ex5 = 0.001
+    n_ex5 = 9
+    starting_ex5 = np.linspace(start=-2.5,stop=2.5,num=n_ex5)
+    time_mesh_ex5 = np.linspace(start=0,stop=time_ex2,num=int(time_ex2/dt_ex5))
+    sims_ex5 = deterministic_dyson(dt=dt_ex5,time=time_ex2,n=n_ex5,starting=starting_ex5)
+    for i in range(n_ex5):
+        det_dys_ax[1,1].plot(time_mesh_ex5,sims_ex5[:,i])
+    # Figure parameters
+    for ax in det_dys_ax.flat:
+        ax.set(xlabel=r'time ($t$)', ylabel=r'Position')
+    for ax in det_dys_ax.flat:
+        ax.label_outer()
+
+    # Wishart process
     # # Wishart process
-    # plt.figure(figsize=(7,4))
-    # n_wis = 10
-    # starting_wis = np.linspace(start=0.1, stop=20, num=n_wis)
-    # sims_wis = wishart(dt=0.01,time=10,n=n_wis,starting=starting_wis)
-    # for i in range(n_wis):
-    #     plt.plot(sims_wis[:,i])
+    plt.figure(figsize=(7,4))
+    n_wis = 10
+    starting_wis = np.linspace(start=0.1, stop=20, num=n_wis)
+    sims_wis = wishart(dt=0.01,time=10,n=n_wis,starting=starting_wis)
+    for i in range(n_wis):
+        plt.plot(sims_wis[:,i])
     # plt.show()
 
     # # Deterministic Wishart process
